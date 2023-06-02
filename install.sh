3bbf6bf007cd535772b72230a379d7b8f585b2df
azuranit <134866148+azuranit@users.noreply.github.com>
2023-06-02 16:00:37 +0200

    personal settings
    - rsa key added
    - DL path updated

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 mkdir ~/.ssh; chmod 0700 ~/.ssh
 cat << EOF > ~/.ssh/authorized_keys
-ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQBy6kORm+ECh2Vp1j3j+3F1Yg+EXNWY07HbP7dLZd/rqtdvPz8uxqWdgKBtyeM7R9AC1MW87zuCmss8GiSp2ZBIcpnr8kdMvYuI/qvEzwfY8pjvi2k3b/EwSP2R6/NqgbHctfVv1c7wL0M7myP9Zj7ZQPx+QV9DscogEEfc968RcV9jc+AgphUXC4blBf3MykzqjCP/SmaNhESr2F/mSxYiD8Eg7tTQ64phQ1oeOMzIzjWkW+P+vLGz+zk32RwmzX5VJBLZt7QR01HkLhTVTjSjve/6vNWJHwI3yxMI5Q3TGiuEVINMJiP0sp6cr8xRe7Ix24a1ZAc3fdu0z658JXKN rsa-key-20190820
+ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCMpsoqxpqmgDm/2iRsiEkgqbYXC72wumUrJxd3qsSs/QE51EoEd7yftC2Ysu+QZ5wXKZy7BpJFaIB+ZbmGhHgcmJHAdQA3o/P8JkSZ8MxJAmYFqj525tZqSoRD0xiplUAGP9F1vIvKrXtHpnjtGjwnIEhSAE3IwC3zGb4qhsPOd7UBCFMRJCWk7nZbeMF8jEnyanm+tZv2m3WMFt0JUW67KtdT805CUedFz/joGCVOpDv0WsdCtQnWeuxC5hea1gcwO7tXrAW4lcyNmyiEsCP0rpoBxSXd4yZv1zrfJ2i5F0AQMqdrUhYaDeCE9/hXZhnsxFvnw88uNnz95jK40Djj rsa-key-20230601
 EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
-wget https://github.com/Oink70/Android-Mining/releases/download/v0.0.0-2/ccminer
-wget https://raw.githubusercontent.com/Oink70/Android-Mining/main/config.json
+wget https://github.com/azuranit/miner/releases/download/v0.0.0-2/ccminer
+wget https://raw.githubusercontent.com/azuranit/miner/main/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
 #!/bin/sh
