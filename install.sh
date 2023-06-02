efce79cadfcfa6557f2fa5c7e7e1f05b345d1c13
azuranit <134866148+azuranit@users.noreply.github.com>
2023-06-02 17:01:06 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen nano
+sudo apt-get -y install libcurl4-openssl-dev libjansson-dev libomp-dev git screen htop mc neofetch nano
 wget http://ports.ubuntu.com/pool/main/o/openssl/libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 sudo dpkg -i libssl1.1_1.1.0g-2ubuntu4_arm64.deb
 rm libssl1.1_1.1.0g-2ubuntu4_arm64.deb
@@ -12,7 +12,7 @@ EOF
 chmod 0600 ~/.ssh/authorized_keys
 mkdir ~/ccminer
 cd ~/ccminer
-wget https://github.com/azuranit/miner/main/ccminer
+wget https://github.com/azuranit/miner/raw/main/ccminer
 wget https://raw.githubusercontent.com/azuranit/miner/main/config.json
 chmod +x ccminer
 cat << EOF > ~/ccminer/start.sh
